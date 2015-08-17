09a30693cb96550b5bb94d1ebf7fb2843740c4cc
Matthew Mosesohn <mmosesohn@mirantis.com>
2015-08-17 13:18:15 +0200

    Initial commit
    Change-Id: Ieb31431ee19d74b101fbd248990f99b57430ea2d

---
+++ deployment_scripts/database-haproxy.pp
@@ -0,0 +1,21 @@
+notice('MODULAR: haproxy-database.pp')
+
+$is_primary_controller    = hiera('primary_controller')
+$custom_mysql_setup_class = hiera('custom_mysql_setup_class')
+$mysqld_ipaddresses       = hiera('mysqld_ipaddresses')
+$mysqld_names             = hiera('mysqld_names')
+$database_vip             = hiera('database_vip')
+
+Haproxy::Service        { use_include => true }
+Haproxy::Balancermember { use_include => true }
+
+if ($custom_mysql_setup_class in ['galera', 'percona', 'percona_packages']) {
+  class { 'openstack::ha::mysqld':
+    is_primary_controller => $is_primary_controller,
+    server_names          => $mysqld_names,
+    ipaddresses           => $mysqld_ipaddresses,
+    public_virtual_ip     => $database_vip,
+    internal_virtual_ip   => $database_vip,
+  }
+}
+
